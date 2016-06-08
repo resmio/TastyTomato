53430ca6bbe2633e95278c7aa530965f8ddd21c8
Jan Nash <jan@resmio.com>
2016-06-08 12:10:25 +0200

    Created Xcode project

---
+++ TastyTomato/TastyTomato.h
@@ -0,0 +1,19 @@
+//
+//  TastyTomato.h
+//  TastyTomato
+//
+//  Created by Jan Nash on 6/8/16.
+//  Copyright © 2016 resmio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TastyTomato.
+FOUNDATION_EXPORT double TastyTomatoVersionNumber;
+
+//! Project version string for TastyTomato.
+FOUNDATION_EXPORT const unsigned char TastyTomatoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TastyTomato/PublicHeader.h>
+
+
