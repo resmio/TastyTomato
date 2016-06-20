87d36157996a4dd54cb4d0f2794d4c957b56a340
Jan Nash <jan@resmio.com>
2016-06-20 15:32:57 +0200

    Instead of extending UIColor and possibly running into problems and ambiguities, I decided to subclass it.

--- TastyTomato/Code/Classes/TTColor/TTColor (FromHex_).swift
+++ TastyTomato/Code/Classes/TTColor/TTColor (FromHex_).swift
@@ -1,5 +1,5 @@
 //
-//  UIColor (FromHex_).swift
+//  TTColor (FromHex_).swift
 //  TastyTomato
 //
 //  Created by Jan Nash on 6/8/16.
@@ -10,7 +10,7 @@ import Foundation
 
 
 // MARK: // Internal
-extension UIColor {
+extension TTColor {
     // Input strings should be 6-character Hex strings without the #
     convenience init(fromHex_ hexString: String, withAlpha alpha: CGFloat = 1) {
         self.init(
@@ -22,7 +22,7 @@ extension UIColor {
 
 
 // MARK: // Private
-private extension UIColor {
+private extension TTColor {
     private convenience init(_fromHex hexString: String, withAlpha alpha: CGFloat) {
         let scanner: NSScanner = NSScanner(string: hexString)
         var rgbValue: UInt32 = 0
---
+++ TastyTomato/Code/Classes/TTColor/TTColor.swift
@@ -0,0 +1,14 @@
+//
+//  TTColor.swift
+//  TastyTomato
+//
+//  Created by Jan Nash on 6/20/16.
+//  Copyright © 2016 resmio. All rights reserved.
+//
+
+import UIKit
+
+
+// MARK: // Public
+// MARK: Class Declaration
+public class TTColor: UIColor {}
