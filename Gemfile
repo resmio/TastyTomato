6a6b2eb47113534cf7e13080da30d83113c5e96f
Jan Nash <jan@resmio.com>
2016-07-18 17:58:32 +0200

    Some grouping, dependency updates

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
-gem 'cocoapods'
+gem 'cocoapods', '0.39.0'
+gem 'activesupport', '~> 4.2'
 gem 'cocoapods-deintegrate'
 gem 'synx'
---
+++ TastyTomato/Code/Extensions/Public/UIImage/UIImage (AllImages).swift
@@ -0,0 +1,9 @@
+//
+//  UIImage (AllImages).swift
+//  TastyTomato
+//
+//  Created by Jan Nash on 7/18/16.
+//  Copyright © 2016 resmio. All rights reserved.
+//
+
+import Foundation
