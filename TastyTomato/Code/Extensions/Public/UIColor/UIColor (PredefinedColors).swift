a9ec33135dd72a66b9e58348a15cb16080bfad81
Jan Nash <jan@resmio.com>
2016-08-30 17:27:24 +0200

    Added LightKeyboardBackground color to UIColor (PredefinedColors)

--- TastyTomato/Code/Extensions/Public/UIColor/UIColor (PredefinedColors).swift
+++ TastyTomato/Code/Extensions/Public/UIColor/UIColor (PredefinedColors).swift
@@ -68,19 +68,24 @@ public extension UIColor {
     public static func GrayF7F7F7() -> UIColor {
         return self._GrayF7F7F7
     }
+    
+    public static func LightKeyboardBackground() -> UIColor {
+        return self._LightKeyboardBackround
+    }
 }
 
 
 // MARK: // Private
 private extension UIColor {
-    private static let _RedE62C4F: UIColor =    UIColor(fromHex_: "E62C4F")
-    private static let _YellowF8C150: UIColor = UIColor(fromHex_: "F8C150")
-    private static let _Green22CCAA: UIColor =  UIColor(fromHex_: "22CCAA")
-    private static let _Blue00A7C4: UIColor =   UIColor(fromHex_: "00A7C4")
-    private static let _Blue018EA6: UIColor =   UIColor(fromHex_: "018EA6")
-    private static let _Gray555555: UIColor =   UIColor(fromHex_: "555555")
-    private static let _Gray999999: UIColor =   UIColor(fromHex_: "999999")
-    private static let _GrayCCCCCC: UIColor =   UIColor(fromHex_: "CCCCCC")
-    private static let _GrayDDDDDD: UIColor =   UIColor(fromHex_: "DDDDDD")
-    private static let _GrayF7F7F7: UIColor =   UIColor(fromHex_: "F7F7F7")
+    private static let _RedE62C4F: UIColor =                UIColor(fromHex_: "E62C4F")
+    private static let _YellowF8C150: UIColor =             UIColor(fromHex_: "F8C150")
+    private static let _Green22CCAA: UIColor =              UIColor(fromHex_: "22CCAA")
+    private static let _Blue00A7C4: UIColor =               UIColor(fromHex_: "00A7C4")
+    private static let _Blue018EA6: UIColor =               UIColor(fromHex_: "018EA6")
+    private static let _Gray555555: UIColor =               UIColor(fromHex_: "555555")
+    private static let _Gray999999: UIColor =               UIColor(fromHex_: "999999")
+    private static let _GrayCCCCCC: UIColor =               UIColor(fromHex_: "CCCCCC")
+    private static let _GrayDDDDDD: UIColor =               UIColor(fromHex_: "DDDDDD")
+    private static let _GrayF7F7F7: UIColor =               UIColor(fromHex_: "F7F7F7")
+    private static let _LightKeyboardBackround: UIColor =   UIColor(fromHex_: "CACFD6")
 }
