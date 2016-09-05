80f9ef33329a3b96860005afda47883295d77818
Jan Nash <jan@resmio.com>
2016-09-05 09:28:13 +0200

    Wrote a small nifty extension of UIViewController, so the viewController-containment pattern is easier to use in code

---
+++ TastyTomato/Code/Extensions/Public/UIViewController/UIViewController (EmbedViewController).swift
@@ -0,0 +1,37 @@
+//
+//  UIViewController (EmbedViewController).swift
+//  TastyTomato
+//
+//  Created by Jan Nash on 9/5/16.
+//  Copyright © 2016 resmio. All rights reserved.
+//
+
+import UIKit
+
+
+// MARK: // Public
+public extension UIViewController {
+    public func embedViewController(viewController: UIViewController, inView view: UIView? = nil, inFrame frame: CGRect? = nil) {
+        self._embedViewController(
+            viewController,
+            inView: view,
+            inFrame: frame
+        )
+    }
+}
+
+
+// MARK: // Private
+private extension UIViewController {
+    private func _embedViewController(viewController: UIViewController, inView view: UIView?, inFrame frame: CGRect?) {
+        let containerView: UIView = view ?? self.view
+        let containerFrame: CGRect = frame ?? containerView.frame
+        let vcView: UIView = viewController.view
+        vcView.frame = containerFrame
+        
+        viewController.willMoveToParentViewController(self)
+        self.addChildViewController(viewController)
+        containerView.addSubview(vcView)
+        viewController.didMoveToParentViewController(self)
+    }
+}
