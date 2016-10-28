ac6d3d8aa892cb163500584a42fd6e448f6795af
Jan Nash <jan@resmio.com>
2016-10-28 14:46:19 +0200

    Added UIView scaleByFactor extension

---
+++ TastyTomato/Code/Extensions/Public/UIView/UIView (ScaleByFactor).swift
@@ -0,0 +1,29 @@
+//
+//  UIView (ScaleByFactor).swift
+//  TastyTomato
+//
+//  Created by Jan Nash on 10/28/16.
+//  Copyright © 2016 resmio. All rights reserved.
+//
+
+import UIKit
+
+
+// MARK: // Public
+public extension UIView {
+    public func scale(byFactor factor: CGFloat) {
+        self._scale(byFactor: factor)
+    }
+}
+
+
+// MARK: // Private
+private extension UIView {
+    func _scale(byFactor factor: CGFloat) {
+        if factor == 0 || factor == 1 {
+        return
+        }
+        
+        self.size.scaleByFactor(factor)
+    }
+}
