09f2c6dee0de659ac101eff9a6940d9a4a4dd23a
Jan Nash <jan@resmio.com>
2016-11-29 14:33:42 +0100

    Added return to the zoom-convenience functions, so we can conditionally call code if the zoomScale is already at the desired value and thus the scrollView doesn't zoom

--- TastyTomato/Code/Extensions/Public/UIScrollView/UIScrollView (ZoomingConvenience).swift
+++ TastyTomato/Code/Extensions/Public/UIScrollView/UIScrollView (ZoomingConvenience).swift
@@ -29,18 +29,49 @@ extension UIScrollView {
 }
 
 
+// MARK: // Internal
+internal extension UIScrollView {
+    func zoomOut_(animated: Bool = true) -> Bool {
+        return self._zoomOut(animated: animated)
+    }
+    
+    func zoomIn_(animated: Bool = true) -> Bool {
+        return self._zoomIn(animated: animated)
+    }
+    
+    func zoom_(to zoomScale: CGFloat, animated: Bool = true) -> Bool {
+        return self._zoom(to: zoomScale, animated: animated)
+    }
+}
+
+
 // MARK: // Private
 private extension UIScrollView {
-    func _zoomOut(animated: Bool) {
-        self.setZoomScale(self.minimumZoomScale, animated: animated)
+    @discardableResult func _zoomOut(animated: Bool) -> Bool {
+        if self.zoomScale != self.minimumZoomScale {
+            self.setZoomScale(self.minimumZoomScale, animated: animated)
+            return true
+        } else {
+            return false
+        }
     }
     
-    func _zoomIn(animated: Bool) {
-        self.setZoomScale(self.maximumZoomScale, animated: animated)
+    @discardableResult func _zoomIn(animated: Bool) -> Bool {
+        if self.zoomScale != self.maximumZoomScale {
+            self.setZoomScale(self.maximumZoomScale, animated: animated)
+            return true
+        } else {
+            return false
+        }
     }
     
-    func _zoom(to zoomScale: CGFloat, animated: Bool) {
-        self.setZoomScale(zoomScale, animated: animated)
+    @discardableResult func _zoom(to zoomScale: CGFloat, animated: Bool) -> Bool {
+        if self.zoomScale == zoomScale {
+            self.setZoomScale(zoomScale, animated: animated)
+            return true
+        } else {
+            return false
+        }
     }
     
     func _zoom(to zoomPoint: CGPoint, with scale: CGFloat, animated: Bool) {
