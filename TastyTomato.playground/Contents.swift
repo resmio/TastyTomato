9ded274f84e7b72d5b0bb617e86a1eb2e4ef9de4
Jan Nash <jan@resmio.com>
2017-03-25 10:40:44 +0100

    Add more buttons to the playground

--- TastyTomato.playground/Contents.swift
+++ TastyTomato.playground/Contents.swift
@@ -8,18 +8,43 @@ import TastyTomato
 // // Setup playground execution and liveView // //
 PlaygroundPage.current.needsIndefiniteExecution = true
 let liveView: UIView = UIView(
-    frame: CGRect(x: 0, y: 0, width: 300, height: 800)
+    frame: CGRect(x: 0, y: 0, width: 300, height: 2000)
 )
 PlaygroundPage.current.liveView = liveView
 liveView.backgroundColor = .white
 liveView.tintColor = .white
 // // // // // // // // // // // // // // // // //
 
+var viewsToDisplay: [UIView] = []
+
+
+// ButtonClasses
+let baseButton: BaseButton = BaseButton_()
+baseButton.autoAdjustWidthOnTitleSet = true
+baseButton.setTitle("BaseButton")
+baseButton.tintColor = .black
+viewsToDisplay.append(baseButton)
 
 
 let filledButton: FilledButton = FilledButton_()
 filledButton.setTitle("FilledButton")
 filledButton.setFillColor(.black)
+filledButton.setHighlightedAlpha(0.01)
+viewsToDisplay.append(filledButton)
+
+
+let shortcutBarButton: ShortcutBarButton = ShortcutBarButton_()
+shortcutBarButton.setTitle("ShortcutBarButton")
+viewsToDisplay.append(shortcutBarButton)
+
+
+let textButton: TextButton = TextButton_()
+textButton.setTitle("TextButton")
+textButton.underlined = true
+viewsToDisplay.append(textButton)
+
+
+// CustomViews
 
 
 
@@ -67,10 +92,10 @@ filledButton.setFillColor(.black)
 
 
 
-
-
-
-liveView.addSubview(filledButton)
-filledButton.top = 10
-filledButton.left = 10
-
+var nextViewTop: CGFloat = 50
+for view in viewsToDisplay {
+    liveView.addSubview(view)
+    view.left = 10
+    view.top = nextViewTop
+    nextViewTop = view.bottom + 50
+}
