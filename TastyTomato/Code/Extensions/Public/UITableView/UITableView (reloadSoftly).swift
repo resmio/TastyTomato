4408f1fc39cb831d1516cdefc8148294d5feb68e
Jan Nash <jan@resmio.com>
2017-07-11 14:40:33 +0200

    Add UITableView.reloadSoftly() extension

---
+++ TastyTomato/Code/Extensions/Public/UITableView/UITableView (reloadSoftly).swift
@@ -0,0 +1,35 @@
+//
+//  UITableView (reloadSoftly).swift
+//  TastyTomato
+//
+//  Created by Jan Nash on 7/11/17.
+//  Copyright © 2017 resmio. All rights reserved.
+//
+
+import UIKit
+
+
+// MARK: // Public
+public extension UITableView {
+    public func reloadSoftly(animated: Bool = true, duration: TimeInterval = 0.4, additionalAnimations: (() -> Void)? = nil) {
+        self._reloadSoftly(animated: animated, duration: duration, additionalAnimations: additionalAnimations)
+    }
+}
+
+
+// MARK: // Private
+private extension UITableView {
+    func _reloadSoftly(animated: Bool, duration: TimeInterval, additionalAnimations: (() -> Void)?) {
+        let reload: (() -> Void) = {
+            self.beginUpdates()
+            additionalAnimations?()
+            self.endUpdates()
+        }
+        
+        if animated {
+            UIView.animate(withDuration: duration, animations: reload)
+        } else {
+            UIView.performWithoutAnimation(reload)
+        }
+    }
+}
