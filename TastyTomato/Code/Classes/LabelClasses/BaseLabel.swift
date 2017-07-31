514a4428019a0e696ab56941063e20afa7f61b78
Jan Nash <jan@resmio.com>
2017-07-31 12:00:24 +0200

    Implement BaseLabel

--- TastyTomato/Code/Classes/LabelClasses/BaseLabel.swift
+++ TastyTomato/Code/Classes/LabelClasses/BaseLabel.swift
@@ -20,6 +20,15 @@ public extension BaseLabel {
             self._placeholder = newPlaceholder
         }
     }
+    
+    var placeholderTextColor: UIColor? {
+        get {
+            return self._placeholderTextColor
+        }
+        set(newPlaceholderTextColor) {
+            self._placeholderTextColor = newPlaceholderTextColor
+        }
+    }
 }
 
 
@@ -34,10 +43,15 @@ public class BaseLabel: UILabel {
      let label = textField.value(forKey: key) as! UILabel
      label.textColor
      */
-    fileprivate let _placeholderTextColor: UIColor = UIColor(colorLiteralRed: 0, green: 0, blue: 0.098, alpha: 0.22)
+    fileprivate let _defaultPlaceholderTextColor: UIColor = UIColor(colorLiteralRed: 0, green: 0, blue: 0.098, alpha: 0.22)
+    
+    // Private Lazy Variables
+    fileprivate lazy var __placeholderTextColor: UIColor = self._defaultPlaceholderTextColor
     
     // Private Variables
     fileprivate var __placeholder: String?
+    fileprivate var __textColorBackup: UIColor = .black
+    fileprivate var _isShowingPlaceholder: Bool = true
     
     
     // Variable Overrides
@@ -50,35 +64,76 @@ public class BaseLabel: UILabel {
         }
     }
     
-//    public override var textColor: UIColor! {
-//        get {
-//            return self._textColor
-//        }
-//        set(newTextColor) {
-//            self._textColor = newTextColor
-//        }
-//    }
+    public override var textColor: UIColor! {
+        get {
+            return self._textColor
+        }
+        set(newTextColor) {
+            self._textColor = newTextColor
+        }
+    }
 }
 
 
 // MARK: // Private
 // MARK: Computed Variables
 private extension BaseLabel {
-    var _text: String? {
-        get {
-            return super.text
-        }
-        set(newText) {
-            super.text = newText
-        }
-    }
-    
     var _placeholder: String? {
         get {
             return self.__placeholder
         }
-        set(newPlaceholderText) {
-            
+        set(newPlaceholder) {
+            guard newPlaceholder != self.__placeholder else { return }
+            self.__placeholder = newPlaceholder
+            if self._isShowingPlaceholder {
+                super.text = newPlaceholder
+            }
+        }
+    }
+    
+    var _placeholderTextColor: UIColor? {
+        get {
+            return self.__placeholderTextColor
+        }
+        set(newPlaceholderTextColor) {
+            guard newPlaceholderTextColor != self.__placeholderTextColor else { return }
+            let newColor: UIColor = newPlaceholderTextColor ?? self._defaultPlaceholderTextColor
+            self.__placeholderTextColor = newColor
+            if self._isShowingPlaceholder {
+                super.textColor = newColor
+            }
+        }
+    }
+    
+    var _text: String? {
+        get {
+            return self._isShowingPlaceholder ? "" : super.text
+        }
+        set(newText) {
+            guard newText != super.text else { return }
+            if newText.isNilOrEmpty {
+                self._isShowingPlaceholder = true
+                super.text = self.placeholder
+                self.__textColorBackup = super.textColor
+                super.textColor = self.placeholderTextColor
+            } else {
+                self._isShowingPlaceholder = false
+                super.text = newText
+                super.textColor = self.__textColorBackup
+            }
+        }
+    }
+    
+    var _textColor: UIColor! {
+        get {
+            return self._isShowingPlaceholder ? self.__textColorBackup : super.textColor
+        }
+        set(newTextColor) {
+            if self._isShowingPlaceholder {
+                self.__textColorBackup = newTextColor
+            } else {
+                super.textColor = newTextColor
+            }
         }
     }
 }
