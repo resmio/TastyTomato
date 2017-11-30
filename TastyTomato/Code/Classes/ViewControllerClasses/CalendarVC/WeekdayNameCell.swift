02f9eae346f5448c7cccca58680328e35352e5dc
Jan Nash <jan@resmio.com>
2017-11-30 16:26:51 +0100

    Create WeekdayNameCell file

---
+++ TastyTomato/Code/Classes/ViewControllerClasses/CalendarVC/WeekdayNameCell.swift
@@ -0,0 +1,16 @@
+//
+//  WeekdayNameCell.swift
+//  TastyTomato
+//
+//  Created by Jan Nash (resmio) on 30.11.17.
+//  Copyright © 2017 resmio. All rights reserved.
+//
+
+import UIKit
+
+
+// MARK: // Internal
+// MARK: Class Declaration
+class WeekdayNameCell: UICollectionViewCell {
+    
+}
