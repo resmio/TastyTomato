8ab837bd492d9f0d263fe51c6c4749ec4026f244
Jan Nash <jan@resmio.com>
2017-11-30 14:58:27 +0100

    Create CalendarVC file

---
+++ TastyTomato/Code/Classes/ViewControllerClasses/CalendarVC/CalendarVC.swift
@@ -0,0 +1,14 @@
+//
+//  CalendarVC.swift
+//  TastyTomato
+//
+//  Created by Jan Nash (resmio) on 30.11.17.
+//  Copyright © 2017 resmio. All rights reserved.
+//
+
+import UIKit
+
+
+// MARK: // Public
+// MARK: Class Declaration
+public class CalendarVC: UIViewController {}
