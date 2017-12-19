31b8973efd0b02f8a9cec3d02bf5191f6a0e8db4
Jan Nash <jan@resmio.com>
2017-12-19 15:50:52 +0100

    Add backItem to UIBarButtonItem factories

--- TastyTomato/Code/Factories/UIBarButtonItem/UIBarButtonItem (Factories).swift
+++ TastyTomato/Code/Factories/UIBarButtonItem/UIBarButtonItem (Factories).swift
@@ -27,6 +27,10 @@ public extension UIBarButtonItem {
         return ._makeOfflineItem(targetAction: targetAction)
     }
     
+    public static func makeBackItem(targetAction: TargetAction?) -> UIBarButtonItem {
+        return ._makeBackItem(targetAction: targetAction)
+    }
+    
     public static func makeEditItem(targetAction: TargetAction? = nil) -> UIBarButtonItem {
         return ._makeEditItem(targetAction: targetAction)
     }
@@ -53,7 +57,7 @@ public extension UIBarButtonItem {
 // MARK: Buttons
 private extension UIBarButtonItem {
     static func _makeCancelItem(targetAction: TargetAction?) -> UIBarButtonItem {
-        let button: UIButton = self._getButton(targetAction)
+        let button: UIButton = self._getNormalButton(targetAction)
         button.setTitle(NSL_("Cancel"), for: .normal)
         button.sizeToFit()
         button.tintColor = .redE62C4F
@@ -61,62 +65,73 @@ private extension UIBarButtonItem {
     }
     
     static func _makeDeleteItem(targetAction: TargetAction?) -> UIBarButtonItem {
-        let button: UIButton = self._getButton(targetAction)
+        let button: UIButton = self._getNormalButton(targetAction)
         button.setTitle(NSL_("Delete"), for: .normal)
         button.sizeToFit()
         button.tintColor = .redE62C4F
-        button.setTitleColor(.gray999999, for: .disabled)
         return UIBarButtonItem(customView: button)
     }
     
     static func _makeOfflineItem(targetAction: TargetAction?) -> UIBarButtonItem {
         let offlineImage: UIImage = MiscIcon.Offline.asTemplate()
-        let button: UIButton = self._getButton(targetAction)
+        let button: UIButton = self._getNormalButton(targetAction)
         button.setBackgroundImage(offlineImage, for: .normal)
         button.sizeToFit()
         button.tintColor = .redE62C4F
         return UIBarButtonItem(customView: button)
     }
     
+    static func _makeBackItem(targetAction: TargetAction?) -> UIBarButtonItem {
+        let button: ImageTextButton = ImageTextButton()
+        self._configure(button: button, with: targetAction)
+        button.image = ArrowIcon.Left.asTemplate()
+        button.text = NSL_("Back")
+        button.sizeToFit()
+        button.tintColor = .blue00A7C4
+        return UIBarButtonItem(customView: button)
+    }
+    
     static func _makeEditItem(targetAction: TargetAction?) -> UIBarButtonItem {
-        let button: UIButton = self._getButton(targetAction)
+        let button: UIButton = self._getNormalButton(targetAction)
         button.setTitle(NSL_("Edit"), for: .normal)
         button.sizeToFit()
         button.tintColor = .blue00A7C4
-        button.setTitleColor(.gray999999, for: .disabled)
         return UIBarButtonItem(customView: button)
     }
     
     static func _makeNewItem(targetAction: TargetAction?) -> UIBarButtonItem {
-        let button: UIButton = self._getButton(targetAction)
+        let button: UIButton = self._getNormalButton(targetAction)
         button.setTitle(NSL_("New"), for: .normal)
         button.sizeToFit()
         button.tintColor = .blue00A7C4
-        button.setTitleColor(.gray999999, for: .disabled)
         return UIBarButtonItem(customView: button)
     }
     
     static func _makeSaveItem(targetAction: TargetAction?) -> UIBarButtonItem {
-        let button: UIButton = self._getButton(targetAction)
+        let button: UIButton = self._getNormalButton(targetAction)
         button.setTitle(NSL_("Save"), for: .normal)
         button.sizeToFit()
         button.tintColor = .blue00A7C4
-        button.setTitleColor(.gray999999, for: .disabled)
+        
         return UIBarButtonItem(customView: button)
     }
     
     // Helpers
-    private static func _getButton(_ targetAction: TargetAction?) -> UIButton {
+    private static func _getNormalButton(_ targetAction: TargetAction?) -> UIButton {
         let button: UIButton = UIButton(type: .system)
+        self._configure(button: button, with: targetAction)
+        return button
+    }
+    
+    private static func _configure(button: UIButton, with targetAction: TargetAction?) {
         button.backgroundColor = .clear
         button.titleLabel?.font = .systemFont(ofSize: 17)
         button.isExclusiveTouch = true
+        button.setTitleColor(.gray999999, for: .disabled)
         
         if let (target, action) = targetAction {
             button.addTarget(target, action: action, for: .touchUpInside)
         }
-        
-        return button
     }
 }
 
