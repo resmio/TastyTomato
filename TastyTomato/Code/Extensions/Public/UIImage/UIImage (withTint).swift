174b492050a33ce696c01962af763268478a69dd
Jan Nash <jan@resmio.com>
2017-12-19 18:32:16 +0100

    Add UIImage withTint() extension

---
+++ TastyTomato/Code/Extensions/Public/UIImage/UIImage (withTint).swift
@@ -0,0 +1,42 @@
+//
+//  UIImage (withTint).swift
+//  TastyTomato
+//
+//  Created by Jan Nash (resmio) on 19.12.17.
+//  Copyright © 2017 resmio. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+
+
+// MARK: // Public
+public extension UIImage {
+    public func withTint(color: UIColor) -> UIImage? {
+        return self._withTint(color: color)
+    }
+}
+
+
+// MARK: // Private
+private extension UIImage {
+    func _withTint(color: UIColor) -> UIImage? {
+        let size: CGSize = self.size
+        
+        UIGraphicsBeginImageContextWithOptions(size, false, self.scale)
+        guard let cgImage: CGImage = self.cgImage, let context: CGContext = UIGraphicsGetCurrentContext() else {
+            return nil
+        }
+        
+        let drawRect: CGRect = size.asCGRect()
+        context.clip(to: drawRect, mask: cgImage)
+        color.setFill()
+        UIRectFill(drawRect)
+        self.draw(in: drawRect)
+        
+        let tintedImage: UIImage? = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return tintedImage
+    }
+}
