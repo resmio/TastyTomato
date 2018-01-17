ca9eda9cbc9d2772a5376006c314b7f8a98e30df
Jan Nash <jan@resmio.com>
2018-01-17 16:19:33 +0100

    Create UIFont extension with predefined fonts

---
+++ TastyTomato/Code/Extensions/Public/UIFont/UIFont (PredefinedFonts).swift
@@ -0,0 +1,49 @@
+//
+//  UIFont (PredefinedFonts).swift
+//  TastyTomato
+//
+//  Created by Jan Nash (resmio) on 17.01.18.
+//  Copyright © 2018 resmio. All rights reserved.
+//
+
+import UIKit
+
+
+// MARK: // Public
+public extension UIFont {
+    public static var xxxs: UIFont {
+        return .systemFont(ofSize: 12)
+    }
+    
+    public static var xxs: UIFont {
+        return .systemFont(ofSize: 14)
+    }
+    
+    public static var xs: UIFont {
+        return .systemFont(ofSize: 15)
+    }
+    
+    public static var s: UIFont {
+        return .systemFont(ofSize: 16)
+    }
+    
+    public static var m: UIFont {
+        return .systemFont(ofSize: 17)
+    }
+    
+    public static var l: UIFont {
+        return .systemFont(ofSize: 18)
+    }
+    
+    public static var xl: UIFont {
+        return .systemFont(ofSize: 20)
+    }
+    
+    public static var xxl: UIFont {
+        return .systemFont(ofSize: 22)
+    }
+    
+    public static var xxxl: UIFont {
+        return .systemFont(ofSize: 24)
+    }
+}
