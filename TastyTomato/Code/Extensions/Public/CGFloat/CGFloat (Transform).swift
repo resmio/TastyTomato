d9e891bc4c5680dfc38e8cb2800682ba1eb8edad
Jan Nash <jan@resmio.com>
2018-08-23 12:03:01 +0200

    Add CGSize and CGFloat transformation extension

---
+++ TastyTomato/Code/Extensions/Public/CGFloat/CGFloat (Transform).swift
@@ -0,0 +1,16 @@
+//
+//  CGFloat (Transform).swift
+//  TastyTomato
+//
+//  Created by Jan Nash (resmio) on 23.08.18.
+//  Copyright © 2018 resmio. All rights reserved.
+//
+
+import Foundation
+
+
+// MARK: // Public
+// MARK: -
+public extension CGFloat {
+    public typealias Transform = (CGFloat) -> CGFloat
+}
