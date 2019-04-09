f07ee31e5a3417d3b6be5942a58b4d55e6becbe6
Jan Nash <jan@resmio.com>
2019-04-09 19:36:12 +0200

    Update Hashable conformance implementation

--- TastyTomato/Code/Classes/LayerClasses/GridLayer.swift
+++ TastyTomato/Code/Classes/LayerClasses/GridLayer.swift
@@ -478,8 +478,8 @@ private func ==(lhs: _PositionIndex, rhs: _PositionIndex) -> Bool {
 
 // MARK: Hashable
 extension _PositionIndex: Hashable {
-    var hashValue: Int {
-        return self.value.hashValue
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(self.value)
     }
 }
 
