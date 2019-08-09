c1223204b9e9146c90e53c1d536ff26250de3217
Jan Nash <jan@resmio.com>
2019-08-09 16:04:22 +0200

    Create BaseSwitch

---
+++ TastyTomato/Code/Types/BaseClasses/BaseSwitch.swift
@@ -0,0 +1,36 @@
+//
+//  BaseSwitch.swift
+//  TastyTomato
+//
+//  Created by Jan Nash (resmio) on 09.08.19.
+//  Copyright © 2019 resmio. All rights reserved.
+//
+
+import Foundation
+
+
+// MARK: // Public
+// MARK: Class Declaration
+public class BaseSwitch: UISwitch {
+    // Required Init
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self._init()
+    }
+    
+    // Override Init
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        self._init()
+    }
+    
+    // Common Init
+    private func _init() {
+        self.setColorAdjustment({
+            let swtch: BaseSwitch? = $0 as? BaseSwitch
+            swtch?.tintColor = ColorScheme.lines.switchBorder
+            swtch?.onTintColor = ColorScheme.background.switchOn
+            swtch?.backgroundColor = ColorScheme.background.switchOff
+        })
+    }
+}
