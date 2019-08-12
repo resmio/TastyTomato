4ef61029c94a72311d3521f575d0cf56ba97a854
Jan Nash <jan@resmio.com>
2019-08-12 13:28:42 +0200

    Override placeholder in BaseTextField

--- TastyTomato/Code/Types/BaseClasses/BaseTextField.swift
+++ TastyTomato/Code/Types/BaseClasses/BaseTextField.swift
@@ -12,26 +12,46 @@ import UIKit
 // MARK: // Public
 // MARK: Class Declaration
 public class BaseTextField: UITextField {
+    // Required Init
     public required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         self._init()
     }
     
+    // Override Init
     public override init(frame: CGRect) {
         super.init(frame: frame)
         self._init()
     }
     
+    // Init
     public init() {
         super.init(frame: CGRect(x: 0, y: 0, width: 0, height: 44))
         self._init()
     }
     
-    // Common Init
-    private func _init() {
+    // Placeholder Override
+    public override var placeholder: String? {
+        get { return self.attributedPlaceholder?.string }
+        set { self._setPlaceholder(newValue) }
+    }
+}
+
+
+// MARK: // Private
+// MARK: Common Init
+private extension BaseTextField {
+    func _init() {
         self.setColorAdjustment({
             $0.layer.borderColor = ColorScheme.lines.borderUnfocussed.cgColor
-            ($0 as? UITextField)?.textColor = ColorScheme.text.default
+            let textField: UITextField? = $0 as? UITextField
+            textField?.textColor = ColorScheme.text.default
+            
+            if let placeholder: String = textField?.placeholder {
+                textField?.attributedPlaceholder = NSAttributedString(
+                    string: placeholder, attributes: [.foregroundColor: ColorScheme.text.placeholder]
+                )
+            }
         })
         
         let layer: CALayer = self.layer
@@ -49,5 +69,20 @@ public class BaseTextField: UITextField {
         self.font = .s
         self.autocapitalizationType = .none
         self.autocorrectionType = .no
+    }
+}
+
+
+// MARK: Set Placeholder
+private extension BaseTextField {
+    func _setPlaceholder(_ placeholder: String?) {
+        guard let placeholder: String = placeholder else {
+            self.attributedPlaceholder = nil
+            return
+        }
+        
+        self.attributedPlaceholder = NSAttributedString(
+            string: placeholder, attributes: [.foregroundColor: ColorScheme.text.placeholder]
+        )
     }
 }
