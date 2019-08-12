4d02e0b0cdfaf3d66ddbd776420b6663e54805eb
Jan Nash <jan@resmio.com>
2019-08-12 13:08:27 +0200

    Improve BaseTextField

--- TastyTomato/Code/Types/BaseClasses/BaseTextField.swift
+++ TastyTomato/Code/Types/BaseClasses/BaseTextField.swift
@@ -14,23 +14,29 @@ import UIKit
 public class BaseTextField: UITextField {
     public required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        self._setup()
+        self._init()
     }
     
     public override init(frame: CGRect) {
         super.init(frame: frame)
-        self._setup()
+        self._init()
     }
-}
-
-
-// MARK: // Private
-// MARK: Setup
-private extension BaseTextField {
-    func _setup() {
-        self.layer.cornerRadius = 4
-        self.layer.borderWidth = 1
-        self.layer.borderColor = UIColor.grayDDDDDD.cgColor
+    
+    public init() {
+        super.init(frame: CGRect(x: 0, y: 0, width: 0, height: 44))
+        self._init()
+    }
+    
+    // Common Init
+    private func _init() {
+        self.setColorAdjustment({
+            $0.layer.borderColor = ColorScheme.lines.borderUnfocussed.cgColor
+            ($0 as? UITextField)?.textColor = ColorScheme.text.default
+        })
+        
+        let layer: CALayer = self.layer
+        layer.cornerRadius = 4
+        layer.borderWidth = 1
         self.clipsToBounds = true
         
         let paddingViewSize: CGSize = CGSize(width: 10, height: 1)
@@ -40,8 +46,7 @@ private extension BaseTextField {
         self.rightView = UIView(size: paddingViewSize)
         self.rightViewMode = .always
         
-        self.font = .m
-        self.textColor = .gray555555
+        self.font = .s
         self.autocapitalizationType = .none
         self.autocorrectionType = .no
     }
