fa7930c554ec4cf80ad2fcce6d392014e3be15db
Jan Nash <jan@resmio.com>
2019-08-13 14:40:43 +0200

    Add makeCreateButton to FilledButton

--- TastyTomato/Code/Types/ButtonClasses/FilledButton.swift
+++ TastyTomato/Code/Types/ButtonClasses/FilledButton.swift
@@ -25,6 +25,10 @@ public extension FilledButton {
         return .makeBlueButton(title: NSL_("Select"))
     }
     
+    static func makeCreateButton() -> FilledButton {
+        return ._makeCreateButton()
+    }
+    
     static func makeDeleteButton() -> FilledButton {
         return ._makeDeleteButton()
     }
@@ -57,6 +61,19 @@ private extension FilledButton {
             let background: ColorScheme.Background = ColorScheme.background
             let dimmed: Bool = filledButton._isDimmed
             let fillColor: UIColor = dimmed ? background.filledButtonDimmed : background.filledButton
+            filledButton.setColor(fillColor, for: .normal)
+            filledButton.setColor(fillColor.withAlpha(filledButton.highlightedAlpha), for: .highlighted)
+            filledButton.setTitleColor(ColorScheme.text.filledButton, for: .normal)
+        })
+        return button
+    }
+    
+    static func _makeCreateButton() -> FilledButton {
+        let button: FilledButton = ._makeDefaultButton(title: NSL_("Create"))
+        button.titleLabel!.font = .xs
+        button.setColorAdjustment({
+            guard let filledButton: FilledButton = $0 as? FilledButton else { return }
+            let fillColor: UIColor = ColorScheme.background.createButton
             filledButton.setColor(fillColor, for: .normal)
             filledButton.setColor(fillColor.withAlpha(filledButton.highlightedAlpha), for: .highlighted)
             filledButton.setTitleColor(ColorScheme.text.filledButton, for: .normal)
