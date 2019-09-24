3a7fa22a1be78cb42631d43b1aaca7147442bd25
Jan Nash <jan@resmio.com>
2019-09-24 14:36:07 +0200

    Cleanup predefined colors

--- TastyTomato/Code/Extensions/Public/UIColor/UIColor (PredefinedColors).swift
+++ TastyTomato/Code/Extensions/Public/UIColor/UIColor (PredefinedColors).swift
@@ -9,41 +9,14 @@
 import UIKit
 
 
-// TODO: Define these colors!
-// Maybe we can generalize how a color should change when it's highlighted, 
-// then we could implement is as a method .highlighted() -> TTColor
-@objc public extension UIColor {
-    static var highlightedBlue: UIColor {
-        return self.blue00A7C4.withAlpha(0.3)
-    }
-    
-    static var highlightedRed: UIColor {
-        return self.redE62C4F.withAlpha(0.3)
-    }
-    
-}
-
-
 // MARK: // Public
 public extension UIColor {
     static var brown7B4900: UIColor {
         return UIColor(fromHex_: "7B4900")
     }
     
-    static var redBC213F: UIColor {
-        return UIColor(fromHex_: "BC213F")
-    }
-    
-    @objc static var redE62C4F: UIColor {
+    static var redE62C4F: UIColor {
         return UIColor(fromHex_: "E62C4F")
-    }
-    
-    static var orangeD97B08: UIColor {
-        return UIColor(fromHex_: "D97B08")
-    }
-    
-    static var orangeFF910A: UIColor {
-        return UIColor(fromHex_: "FF910A")
     }
     
     static var yellowD7A43B: UIColor {
@@ -58,7 +31,7 @@ public extension UIColor {
         return UIColor(fromHex_: "FFF2D8")
     }
     
-    @objc static var green22CCAA: UIColor {
+    static var green22CCAA: UIColor {
         return UIColor(fromHex_: "22CCAA")
     }
     
@@ -66,7 +39,7 @@ public extension UIColor {
         return UIColor(fromHex_: "21B498")
     }
     
-    @objc static var blue00A7C4: UIColor {
+    static var blue00A7C4: UIColor {
         return UIColor(fromHex_: "00A7C4")
     }
     
@@ -98,12 +71,8 @@ public extension UIColor {
         return UIColor(fromHex_: "222222")
     }
     
-    @objc static var gray555555: UIColor {
+    static var gray555555: UIColor {
         return UIColor(fromHex_: "555555")
-    }
-    
-    static var gray909295: UIColor {
-        return UIColor(fromHex_: "909295")
     }
     
     static var gray999999: UIColor {
@@ -114,12 +83,8 @@ public extension UIColor {
         return UIColor(fromHex_: "CCCCCC")
     }
     
-    @objc static var grayDDDDDD: UIColor {
+    static var grayDDDDDD: UIColor {
         return UIColor(fromHex_: "DDDDDD")
-    }
-    
-    static var grayEEEEEE: UIColor {
-        return UIColor(fromHex_: "EEEEEE")
     }
     
     static var gray3E4862: UIColor {
@@ -132,22 +97,6 @@ public extension UIColor {
     
     static var whiteFFFFFF: UIColor {
         return UIColor(fromHex_: "FFFFFF")
-    }
-    
-    static var black000000: UIColor {
-        return UIColor(fromHex_: "000000")
-    }
-    
-    static var purple543EAC: UIColor {
-        return UIColor(fromHex_: "543EAC")
-    }
-    
-    static var purple7563BC: UIColor {
-        return UIColor(fromHex_: "7563BC")
-    }
-    
-    static var pinkFCE9ED: UIColor {
-        return UIColor(fromHex_: "FCE9ED")
     }
     
     static var lightKeyboardBackground: UIColor {
